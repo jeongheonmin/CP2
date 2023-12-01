ff26997e998da8f800bc68e38ce3648fce736547
jeonghyeonmin1 <hyunmin9808@naver.com>
2023-12-01 19:13:13 +0900

    stack 예제 수정

--- Stack_ex.py
+++ Stack_ex.py
@@ -1,29 +1,62 @@
-t = int(input())
+#stack을 이용한 괄호의 매칭 검사
+#여는 괄호는 push, 닫는 괄호는 pop 해주는 방식 
+str=input('괄호 입력 :')
+s=[None for _ in range(len(str))]
+top=-1
 
-for i in range(t):
-  stack = []
-  a=input()
-  for j in a:
-    if j=='(':
-      stack.append(j)
-    elif j==')':
-      if stack:
-        stack.pop()
-      else:
-        print('NO')
-        break
+def pop():
+  global SIZE, s, top
+  if(isStackEmpty()):
+    print("스택이 비었습니다.")
+    return None
+  data=s[top]
+  s[top]=None
+  top-=1
+  return data
 
-    if j=='{':
-      stack.append(j)
-    elif j=='}':
-      if stack:
-        stack.pop()
-      else:
-        print('NO')
-        break
+def push(data):
+  global SIZE,s,top
+  
+  if(isStackFull()):
+    print('스택이 꽉 찼습니다.')
+    return
+  
+  top+=1
+  s[top]=data
 
-  else:
-    if not stack:
-      print('YES')
+def peek():
+  global SIZE, s, top
+  if(isStackEmpty()):
+    print('스택이 비었습니다')
+    return 
+  return s[top]
+
+
+for p in str:
+  if p=='(' or p=='{' or p=='[':
+    push(p)
+  elif p==')':
+    if s[top]=='(':
+      pop()
     else:
-      print("NO")
+      print('괄호가 올바르지 않음')
+      break
+    
+  elif p=='}':
+    if s[top]=='{':
+      pop()
+    else:
+      print('괄호가 올바르지 않음')
+      break
+    
+  elif p==']':
+    if s[top]=='[':
+      pop()
+    else:
+      print('괄호가 올바르지 않음')
+      break
+print(s)
+if top==-1:
+  print('True')
+else:
+  print('False')
